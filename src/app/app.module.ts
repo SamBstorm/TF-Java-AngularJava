0ea21175a8c5abc7f78407fe0a499d73d2786fd3
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-25 12:07:20 +0200

    Exercice 01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoRoutingModule } from './demo/demo-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { ExerciceRoutingModule } from './exercice/exercice-routing.module';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DemoRoutingModule,
+    ExerciceRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
